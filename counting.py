0100bd93d2250ad8c52b04f148861d0a0200fd53
Artem Sychov <artem@sychov.dev>
2023-11-18 22:07:58 +0100

    add some play

--- counting.py
+++ counting.py
@@ -11,3 +11,28 @@ print('Counting...')
 for word in words:
     counts[word] = counts.get(word, 0) + 1
 print('Counts', counts)
+
+# Defenitive Loops and Dictionaries
+for key in counts:
+    print(key, counts[key])
+
+for k,v in counts.items():
+    print(k, v)
+
+# name = input('Enter file:')
+# handle = open(name)
+
+# counts = dict()
+# for line in handle:
+#   words = line.split()
+#   for word in words:
+#        counts[word] = counts.get(word,0) + 1
+
+# bigcount = None
+# bigword = None
+# for word,count in counts.items():
+#   if bigcount is None or count > bigcount:
+#        bigword = word
+#        bigcount = count
+
+# print(bitword, bigcount)
