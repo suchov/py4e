f4d1af579c09ea6de8b514596b609ccf0c810724
Artem Sychov <artem@sychov.dev>
2023-12-09 18:42:17 +0100

    unicode example file

---
+++ unicoding.py
@@ -0,0 +1,17 @@
+# .encode() - convert to byte code
+
+# .decode() - convert from byte code to unicode
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET https://data.pr4e.org/romeo.txt HTTPS/1.0\n\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
