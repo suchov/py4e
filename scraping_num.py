930a2ae8ab6e981c7621efde6b4c22a0dd552998
Artem Sychov <artem@sychov.dev>
2023-12-10 18:47:44 +0100

    finish scaping numbers and adding them task

--- scraping_num.py
+++ scraping_num.py
@@ -1,33 +1,25 @@
-# https://py4e-data.dr-chuck.net/comments_42.html
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
-import re
-
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-# url = "https://py4e-data.dr-chuck.net/comments_42.htm"
-url = 'http://py4e-data.dr-chuck.net/comments_1128050.html'
-# url = input('Enter - ')
+url = input('Enter - ')
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
+sum = 0
 
 # Retrieve all of the anchor tags
 tags = soup('span')
-numlist = list()
 for tag in tags:
     # Look at the parts of a tag
-    y = str(tag)
-    num = re.findall('[0-9]+', y)
-    numlist = numlist + num
-
-sum = 0
-for i in numlist:
-    sum = sum + int(i)
-
-print(sum)
+    print(tag.contents[0])
+    sum = sum + int(tag.contents[0])
+print("Sum =", sum)
